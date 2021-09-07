8cef57a1e87fbb754a629451045291431eb202a9
link38083 <igo3505@gmail.com>
2021-09-07 17:27:30 +0300

    Антидороу работает Погода работает збсь, если не вводить /weather

--- antidorou_bot_test.py
+++ antidorou_bot_test.py
@@ -1,15 +1,13 @@
-import os
 import random
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-bot_token = os.getenv("ANTIDOROU")
-bot = Bot(str(bot_token))
+bot = Bot(token='1980859217:AAHKffL93dOcN_XdJA89Dqf2YgxFPb14__Q')
 dp = Dispatcher(bot)
 
 # ОТВЕТ
-answer = ["иди нахуй", "да нахуй иди", "да, господин?", "дороу, дороу", "ты, может, закончишь уже?"]
+answer = ["иди нахуй", "да нахуй иди", "да, господин?", "дороу, дороу", "ты может закончишь уже?"]
 @dp.message_handler(regexp='дороу')
 @dp.message_handler(regexp='драсти')
 async def otvet(message: types.Message):
--- weather_bot_test.py
+++ weather_bot_test.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import datetime
 #import random
@@ -6,8 +7,10 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 #from aiogram.utils.helper import Helper, HelperMode, ListItem
 
-bot = Bot(token='1979372262:AAF9Qpx-2CY3do3iG8isMr_pTvBsnmO2qQ8')
-open_weather_token = '73bfc1f783f8b62876dc954705fa8475'
+# BOT
+bot_token = os.getenv("TELETOKEN")
+open_weather_token = os.getenv("WEATHERTOKEN")
+bot = Bot(bot_token)
 dp = Dispatcher(bot)
 
 # /start
@@ -15,35 +18,42 @@ dp = Dispatcher(bot)
 async def start_command(message: types.Message):
     await message.reply("Hello!")
 
+# /help
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.Message):
+    await message.reply("Данный бот выдает погоду в любом городе.\n"
+                        "Введи текст в формате /weather *город*."
+        )
+
 # /weather
 @dp.message_handler(commands=['weather'])
 async def weather_command(message: types.Message):
-    await message.reply('Город введи')
+    text = message.text
+    command, text_without_command = text.split(None, maxsplit=1)
+    try:
+        r = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={text_without_command}&appid={open_weather_token}&units=metric"
+        )
+        data = r.json()
 
-    @dp.message_handler()
-    async def get_weather(message: types.Message):
-        try:
-            r = requests.get(
-                f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric"
-            )
-            data = r.json()
+        city = data['name']
+        cur_weather = data['main']['temp']
+        feels_like = data['main']['feels_like']
+        humidity = data['main']['humidity']
+        pressure = data['main']['pressure']/1.333
+        wind = data['wind']['speed']
+        date = datetime.datetime.now().strftime('%Y-%m-%d')
 
-            city = data['name']
-            cur_weather = data['main']['temp']
-            feels_like = data['main']['feels_like']
-            humidity = data['main']['humidity']
-            pressure = data['main']['pressure']
-            wind = data['wind']['speed']
-            date = datetime.datetime.now().strftime('%Y-%m-%d')
-
-            return await message.reply(
-                f'***Погода на {date}***\n'
-                f'Погода в городе: {city}\nТемпература: {cur_weather}C\nОщущается как: {feels_like}C\n'
-                f'Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст.\nВетер: {wind} м/c\n'
-                f'Хорошего дня, ебать'
-            )
-        except Exception as ex:
-            return await message.reply('Городом ошибся')
+        return await message.reply(
+            f'***Погода на {date}***\n'
+            f'Погода в городе: {city}\nТемпература: {cur_weather}C\nОщущается как: {feels_like}C\n'
+            f'Влажность: {humidity}%\nДавление: {pressure:.2f} мм.рт.ст.\nВетер: {wind} м/c\n'
+            f'Хорошего дня, ебать'
+        )
+    except Exception as ex:
+        return await message.reply('Городом ошибся')
+    except NotEnoughValues:
+        return await message.reply('Введи /weather город')
 
 if __name__ == '__main__':
     executor.start_polling(dp)
