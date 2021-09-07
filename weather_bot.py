148b19918164536dbe684182709bcac68550af31
link38083 <igo3505@gmail.com>
2021-09-07 20:59:24 +0300

    Добавил смайлы в погоду

--- weather_bot.py
+++ weather_bot.py
@@ -30,6 +30,15 @@ async def help_command(message: types.Message):
 async def weather_command(message: types.Message):
     text = message.text
     command, text_without_command = text.split(None, maxsplit=1)
+    code_to_smile = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B"
+    }
     try:
         r = requests.get(
             f"http://api.openweathermap.org/data/2.5/weather?q={text_without_command}&appid={open_weather_token}&units=metric"
@@ -38,6 +47,11 @@ async def weather_command(message: types.Message):
 
         city = data['name']
         cur_weather = data['main']['temp']
+        weather_desc = data['weather'][0]['main']
+        if weather_desc in code_to_smile:
+            wd = code_to_smile[weather_desc]
+        else:
+            wd = "Глянь в окно лучше"
         feels_like = data['main']['feels_like']
         humidity = data['main']['humidity']
         pressure = data['main']['pressure']/1.333
@@ -46,7 +60,7 @@ async def weather_command(message: types.Message):
 
         return await message.reply(
             f'***Погода на {date}***\n'
-            f'Погода в городе: {city}\nТемпература: {cur_weather}C\nОщущается как: {feels_like}C\n'
+            f'Погода в городе: {city}\n{wd}\nТемпература: {cur_weather}C°\nОщущается как: {feels_like}C°\n'
             f'Влажность: {humidity}%\nДавление: {pressure:.2f} мм.рт.ст.\nВетер: {wind} м/c\n'
             f'Хорошего дня, ебать'
         )
